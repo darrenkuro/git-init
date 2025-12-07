890f8d49a85069055646f0232a6566433356827b
darrenkuro <odon5ht@gmail.com>
2025-12-07 15:37:49 +0100

    Nixify the program

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "CLI for initializing repos to GitHub";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      desc = self.description;
+    in {
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "git-init";
+        version = "0.1.0";
+
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+
+        # Optional: if your program has tests
+        doCheck = false;
+
+        meta = with pkgs.lib; {
+          description = desc;
+          homepage = "https://github.com/darrenkuro/git-init";
+          license = licenses.mit;
+          maintainers = [maintainers.darrenkuro];
+        };
+      };
+    });
+}
